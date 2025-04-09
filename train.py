258c329b138aeca840b532bd9fb456ec759a76b0
timothycao <tcao.pro@gmail.com>
2025-04-09 18:13:26 -0400

    Created Trainer subclass to add training accuracy metric to logs

--- train.py
+++ train.py
@@ -5,6 +5,47 @@ from peft import LoraConfig
 from model import create_lora_model
 
 
+# HF Trainer does not expose predictions or labels during training (not accessible via callbacks)
+# So this subclass overrides `compute_loss` and `log` methods to manually accumulate them and compute training accuracy
+# Reference discussion: https://discuss.huggingface.co/t/metrics-for-training-set-in-trainer/2461
+# Original compute_loss method: https://github.com/huggingface/transformers/blob/main/src/transformers/trainer.py#L3778
+class CustomTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        # Initialize parent Trainer class
+        super().__init__(*args, **kwargs)
+        
+        # Create lists to accumulate predictions and labels across steps
+        self.train_preds, self.train_labels = [], []
+
+    # Override default compute loss method to reuse its logits and labels
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
+        # Perform forward pass (for logits and loss)
+        outputs = model(**inputs)
+        
+        # Accumulate predictions and labels
+        if 'labels' in inputs and 'logits' in outputs:
+            labels = inputs['labels'].detach().cpu().tolist()
+            preds = outputs.logits.argmax(dim=-1).detach().cpu().tolist()
+            self.train_preds.extend(preds)
+            self.train_labels.extend(labels)
+
+        # Compute and return loss as usual
+        loss = outputs['loss'] if isinstance(outputs, dict) else outputs[0]
+        return (loss, outputs) if return_outputs else loss
+
+    # Override default log method to add training accuracy
+    def log(self, logs, *args, **kwargs):
+        if self.model.training and self.train_preds and self.train_labels:
+            # Compute and log training accuracy for current logging interval
+            logs['accuracy'] = accuracy_score(self.train_labels, self.train_preds)
+            
+            # Reset stored predictions and labels for next interval
+            self.train_preds, self.train_labels = [], []
+        
+        # Call original log method to log default metrics
+        super().log(logs, *args, **kwargs)
+
+
 def preprocess_function(examples, tokenizer):
     return tokenizer(examples['text'], truncation=True, padding=True)
 
@@ -25,7 +66,7 @@ def load_data(tokenizer, test_size=640, seed=42):
     return split_datasets['train'], split_datasets['test']
 
 
-def compute_metrics(pred):
+def compute_eval_metrics(pred):
     labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
     accuracy = accuracy_score(labels, preds)
@@ -43,10 +84,10 @@ def main(model, training_args, checkpoint=None):
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors='pt')
 
     # Initialize Trainer
-    trainer = Trainer(
+    trainer = CustomTrainer(
         model=model,
         args=training_args,
-        compute_metrics=compute_metrics,
+        compute_metrics=compute_eval_metrics,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
         data_collator=data_collator
@@ -74,7 +115,7 @@ if __name__ == '__main__':
     training_args = TrainingArguments(
         # Core training configs
         # num_train_epochs=1,
-        max_steps=200,
+        max_steps=300,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=64,
         optim='adamw_torch',
